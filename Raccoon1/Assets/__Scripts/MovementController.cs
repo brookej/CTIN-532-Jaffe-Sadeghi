cfd8e8203f0e60157004caa6fde97877a6f70389
ahmadmmsadeghi <43358638+ahmadmmsadeghi@users.noreply.github.com>
2018-09-20 18:11:10 -0700

    Raccoon1
    First Commit

---
+++ Raccoon1/Assets/__Scripts/MovementController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovementController : MonoBehaviour
+
+{
+	public float thrust;
+	public Rigidbody2D rb;
+
+
+
+	private void Start()
+
+	{
+
+		rb=GetComponent<Rigidbody2D>();
+
+	}
+
+
+
+	void Update()
+
+	{
+
+		if (Input.GetKey(KeyCode.D))
+
+		{
+			Vector2 vec = new Vector2 (1, 0) * thrust;
+			rb.AddForce(vec,0);
+
+		}
+
+
+
+		if (Input.GetKey(KeyCode.A))
+
+		{
+			Vector2 vec = new Vector2 (-1, 0) * thrust;
+			rb.AddForce(vec,0);
+
+		}
+
+
+
+		if (Input.GetKeyUp(KeyCode.D))
+
+		{
+
+			rb.velocity = new Vector2(0,0);
+
+		}
+
+
+
+		if (Input.GetKeyUp(KeyCode.A))
+
+		{
+
+			rb.velocity = new Vector2(0,0);
+
+		}
+
+	}
+
+}
