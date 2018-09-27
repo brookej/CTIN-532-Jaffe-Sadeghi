94e1602ba17a38c68465a7001c21f3b0865028ac
brookej <33814768+brookej@users.noreply.github.com>
2018-09-27 16:34:49 -0700

    restart button + minor adjustments

---
+++ Raccoon1/Assets/__Scripts/RestartButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour {
+
+    public void ReloadScene()
+    {
+        StartCoroutine(SceneReload());
+    }
+
+    IEnumerator SceneReload()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
