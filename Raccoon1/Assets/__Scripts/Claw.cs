416e7565f5602fef2d66107127b0f1e301ae5009
brookej <33814768+brookej@users.noreply.github.com>
2018-09-27 21:25:50 -0700

    Added Claw action/sound, trash spawner, bg, new graphics! a lot of stuff

---
+++ Raccoon1/Assets/__Scripts/Claw.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Claw : MonoBehaviour {
+    public AudioSource clack;
+    public AudioClip clackclip;
+    public GameObject clawR;
+    public GameObject clawL;
+    public GameObject closedClaw;
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clawR.SetActive(false);
+            clawL.SetActive(false);
+            closedClaw.SetActive(true);
+            clack.PlayOneShot(clackclip);
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            clawR.SetActive(true);
+            clawL.SetActive(true);
+            closedClaw.SetActive(false);
+        }
+     }
+}
---
+++ Raccoon1/Assets/__Scripts/TrashBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBehavior : MonoBehaviour
+{
+    public AudioSource collectSound;
+    GameObject raccoonArm;
+    float distanceToHand;
+    public GameObject TrashRespawn;
+    public float reachRange;
+    public TrashSpawn spawn;
+
+    void Start()
+    {
+        collectSound = FindObjectOfType<AudioSource>();
+        raccoonArm = GameObject.Find("ClawArm");
+    }
+
+    private void Update()
+    {
+        distanceToHand = Vector3.Distance(raccoonArm.transform.position, transform.position);
+        if (distanceToHand < reachRange)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                collectSound.Play();
+                spawn.SpawnTrash();
+                // To do later-- add point value to score
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
