eecc7f0f6143876885a7f92c672404fe1f5e4dfa
brookej <33814768+brookej@users.noreply.github.com>
2018-10-18 15:43:34 -0700

    Added Controls Screen, button script

---
+++ Raccoon1/Assets/__Scripts/SuspicionManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuspicionManager : MonoBehaviour {
+    private int suspicionValue = 0;
+    private int suspicionMax = 100;
+
+    public void SuspicionDisplay()
+    {
+
+    }
+	
+}
