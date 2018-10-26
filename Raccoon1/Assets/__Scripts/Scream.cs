b493cc302ab6cb0fe619228be5638061ba968a34
ahmadmmsadeghi <43358638+ahmadmmsadeghi@users.noreply.github.com>
2018-10-26 07:54:19 -0700

    Music

---
+++ Raccoon1/Assets/__Scripts/Scream.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scream : MonoBehaviour
+{
+    public AudioSource Scream1;
+    public AudioClip Scream1clip;
+    public GameObject FacePanic;
+    public GameObject FaceNormal;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject == FaceNormal)
+        {
+
+            FaceNormal.SetActive(false);
+            FacePanic.SetActive(true);
+            Scream1.PlayOneShot(Scream1clip);
+            
+        }
+
+        else if (other.gameObject == FacePanic)
+        {
+
+            FaceNormal.SetActive(true);
+            FacePanic.SetActive(false);
+
+        }
+    }
+}
